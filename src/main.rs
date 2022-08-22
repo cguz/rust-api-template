0838473a4cf70ed0129b37fc67fef4a1f35ad3e8
Cesar Augusto Guzman Alvarez <cesar.guzman@visionspace.com>
2022-08-22 19:00:51 +0200

    Fixing format and clippy

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,9 @@ fn index() -> &'static str {
 // API for the Space Trafic Management
 mod stm {
 
+    use serde::{Deserialize, Serialize};
     use std::fs::File;
     use std::path::Path;
-    use serde::{Deserialize, Serialize};
 
     #[derive(Serialize, Deserialize, Debug)]
     struct Object {
@@ -35,9 +35,9 @@ mod stm {
     }
 
     #[derive(Serialize, Deserialize, Debug)]
-    struct CatalogSTM{
-        objects : Vec<Object>,
-        cdm_list : Vec<CDMShort>
+    struct CatalogSTM {
+        objects: Vec<Object>,
+        cdm_list: Vec<CDMShort>,
     }
 
     #[derive(Debug)]
@@ -45,9 +45,9 @@ mod stm {
         NotFound,
     }
 
-    fn find_object(json : &CatalogSTM, name_object: &str) -> Result<String, CatalogError> {
-        for obj in json.objects.iter(){
-            if  obj.name.as_str() == name_object {
+    fn find_object(json: &CatalogSTM, name_object: &str) -> Result<String, CatalogError> {
+        for obj in json.objects.iter() {
+            if obj.name.as_str() == name_object {
                 let result = serde_json::to_string(&obj).unwrap();
                 return Ok(result);
             }
@@ -58,9 +58,9 @@ mod stm {
     /// retrieve information about the satellite
     #[get("/<name_object>")]
     pub fn satellite(name_object: String) -> String {
-
-        let file = File::open(Path::new("./json/").join("data_stm.json")).expect("file should open read only");
-        let json : CatalogSTM = serde_json::from_reader(file).expect("JSON was not well-formatted"); // serde_json::Value
+        let file = File::open(Path::new("./json/").join("data_stm.json"))
+            .expect("file should open read only");
+        let json: CatalogSTM = serde_json::from_reader(file).expect("JSON was not well-formatted"); // serde_json::Value
 
         let object = find_object(&json, &name_object);
 
@@ -73,56 +73,70 @@ mod stm {
     /// retrieve possible CDM of a given satellite
     #[get("/cdm/<name_object>")]
     pub fn cdm_satellite(name_object: String) -> String {
-
-        let file = File::open(Path::new("./json/").join("data_stm.json")).expect("file should open read only");
-        let json : CatalogSTM = serde_json::from_reader(file).expect("JSON was not well-formatted"); // serde_json::Value
+        let file = File::open(Path::new("./json/").join("data_stm.json"))
+            .expect("file should open read only");
+        let json: CatalogSTM = serde_json::from_reader(file).expect("JSON was not well-formatted"); // serde_json::Value
 
         let object = find_object(&json, &name_object);
 
         match object {
             Err(CatalogError::NotFound) => serde_json::to_string(&json).unwrap(),
-            Ok(obj) => { 
-                let json_object : Object = serde_json::from_str(obj.as_str()).expect("JSON was not well-formatted"); 
+            Ok(obj) => {
+                let json_object: Object =
+                    serde_json::from_str(obj.as_str()).expect("JSON was not well-formatted");
                 serde_json::to_string(&json_object.cdm).unwrap()
-            },
+            }
         }
     }
 
     /// retrieve CDM information of two objects
     /// The objects can be either satellites or space debris
     #[get("/cdm/<first_object>/<second_object>")]
-    pub fn cdm_two_objects(first_object: String, second_object : String) -> String {
-        
-        let file = File::open(Path::new("./json/").join("data_stm.json")).expect("file should open read only");
-        let json : CatalogSTM = serde_json::from_reader(file).expect("JSON was not well-formatted"); // serde_json::Value
+    pub fn cdm_two_objects(first_object: String, second_object: String) -> String {
+        let file = File::open(Path::new("./json/").join("data_stm.json"))
+            .expect("file should open read only");
+        let json: CatalogSTM = serde_json::from_reader(file).expect("JSON was not well-formatted"); // serde_json::Value
 
         let object = find_object(&json, &first_object);
 
         match object {
             Err(CatalogError::NotFound) => serde_json::to_string(&json).unwrap(),
-            Ok(obj) => { 
-                let json_object : Object = serde_json::from_str(obj.as_str()).expect("JSON was not well-formatted"); 
-                for obj in json_object.cdm.iter(){
-                    if  obj.as_str() == second_object.as_str() {
+            Ok(obj) => {
+                let json_object: Object =
+                    serde_json::from_str(obj.as_str()).expect("JSON was not well-formatted");
+                for obj in json_object.cdm.iter() {
+                    if obj.as_str() == second_object.as_str() {
                         let str_json_object = serde_json::to_string(&json_object).unwrap();
                         return str_json_object;
                     }
                 }
                 "not found".to_string()
-            },
+            }
         }
     }
 
     /// Retrieve a list of possible CDM
     #[get("/cdm/list")]
     pub fn cdm_list() -> String {
-        let file = File::open(Path::new("./json/").join("data_stm.json")).expect("file should open read only");
-        let json : CatalogSTM = serde_json::from_reader(file).expect("JSON was not well-formatted"); // serde_json::Value
-        
+        let file = File::open(Path::new("./json/").join("data_stm.json"))
+            .expect("file should open read only");
+        let json: CatalogSTM = serde_json::from_reader(file).expect("JSON was not well-formatted"); // serde_json::Value
+
         serde_json::to_string(&json.cdm_list).unwrap()
     }
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).mount("/api/v1/data/stm", routes![stm::satellite, stm::cdm_list, stm::cdm_satellite, stm::cdm_two_objects]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount(
+            "/api/v1/data/stm",
+            routes![
+                stm::satellite,
+                stm::cdm_list,
+                stm::cdm_satellite,
+                stm::cdm_two_objects
+            ],
+        )
+        .launch();
 }
